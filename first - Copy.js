6ba135f7ed20c962e69bbc2af86e78a1a440dc85
Rinkal <rinkaladesara.tops@gmail.com>
2025-06-20 16:02:00 +0530

    one file added

---
+++ first - Copy.js
@@ -0,0 +1,24 @@
+console.log('Test message');
+// 1)Nothing
+a=10
+b=20
+c=a+b
+// document.write('Addition is ',c)
+document.write('Addition is '+c)
+document.write("<br>Minus is ",b-a)
+// 2)Var keyword- we can redeclare & reassign value
+var s = "hello"
+document.write('<br> s is '+s)
+var s= 35
+document.write('<br> s is '+s)
+//3)const keyword - constant - we cant redeclare & reassign value
+const pi = 3.14
+document.write('<br> Pi is '+pi)
+// pi="uiyttrui"
+// document.write('<br> Pi is '+pi)
+//4)let keyword - we cant redeclare but reassign is possible
+let x=100
+document.write('<br> x is '+x)
+ x="Good Afternoon!!!"
+document.write('<br> x is '+x)
+
