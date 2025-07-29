06b42d1a512d7754dc19b578a536ab9e2450cff3
Rinkal <rinkaladesara.tops@gmail.com>
2025-07-29 15:36:35 +0530

    crd

--- crud.js
+++ crud.js
@@ -1,14 +1,18 @@
 let allData = []
+document.getElementById('alldata').innerHTML = ''
 const saveData = ()=>{
     let nm = document.getElementById('name').value
     let age = document.frm.age.value
     let sal = document.frm.salary.value
+    let data = JSON.parse(localStorage.getItem('userinfo'))
+    let len = (data!=null)? data.length+1 : 1;
     let obj = {
-        id:1,
+        id:len,
         name:nm,
         age:age,
         salary:sal
     }
+
     allData.push(obj)
     console.log(allData);
     localStorage.setItem("userinfo",JSON.stringify(allData))
@@ -19,14 +23,34 @@ const dispData = ()=>{
     let data = JSON.parse(localStorage.getItem('userinfo'))
     let tr = ''
     data.map((i)=>{
+        // tr += "<tr><td>"+i.id+"</td>"
+        // tr += "</tr>" 
+        // template literal
         tr += `<tr>
                     <td>${i.id}</td>
                     <td>${i.name}</td>
                     <td>${i.age}</td>
                     <td>${i.salary}</td>
-                    <td>Delete</td>
+                    <td>
+                        <button onclick="delData(${i.id})">Delete</button>
+                    </td>
             </tr>`       
     })
     document.getElementById('alldata').innerHTML = tr
+}
+const delData = (id)=>{
+    let data = JSON.parse(localStorage.getItem('userinfo'))
+    //1 2  4 5 == id=3
+    let res = data.filter((i)=>{
+            return i.id != id
+    })
+    let j=1
+    let newdata = res.map((i)=>{
+            i.id = j++
+            return i
+    })
+    localStorage.setItem("userinfo",JSON.stringify(newdata))
+    document.getElementById('alldata').innerHTML = ''
+    dispData()
 }
 dispData()
