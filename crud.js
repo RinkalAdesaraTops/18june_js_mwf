9ad1d2ef81d5490f5f50e8afc918fa1747a15b85
Rinkal <rinkaladesara.tops@gmail.com>
2025-08-01 16:22:21 +0530

    fg

--- crud.js
+++ crud.js
@@ -4,18 +4,32 @@ const saveData = ()=>{
     let nm = document.getElementById('name').value
     let age = document.frm.age.value
     let sal = document.frm.salary.value
+    let id = document.frm.id.value
     let data = JSON.parse(localStorage.getItem('userinfo'))
     allData = data
     let len = (data!=null)? data.length+1 : 1;
-    let obj = {
-        id:len,
-        name:nm,
-        age:age,
-        salary:sal
+    if(id!=''){
+        //update
+        allData = data.map((i)=>{
+            if(i.id == id){
+                i.name = nm
+                i.age = age
+                i.salary = sal
+            }
+            return i
+        })
+    } else {
+        //insert
+        let obj = {
+            id:len,
+            name:nm,
+            age:age,
+            salary:sal
+        }
+        allData.push(obj)
     }
-
-    allData.push(obj)
-    console.log(allData);
+    document.frm.reset()
+    document.frm.id.value=''
     localStorage.setItem("userinfo",JSON.stringify(allData))
     dispData()
 }
@@ -33,6 +47,7 @@ const dispData = ()=>{
                     <td>${i.age}</td>
                     <td>${i.salary}</td>
                     <td>
+                    <button onclick="editData(${i.id})">Edit</button>
                         <button onclick="delData(${i.id})">Delete</button>
                     </td>
             </tr>`       
@@ -53,5 +68,17 @@ const delData = (id)=>{
     
     localStorage.setItem("userinfo",JSON.stringify(newdata))
     dispData()
+}
+const editData = (id)=>{
+    let data = JSON.parse(localStorage.getItem('userinfo'))
+    let res = data.find((i)=>{
+            return i.id == id
+    })
+    console.log(res);
+    document.getElementById('name').value = res.name
+    document.frm.age.value = res.age
+    document.frm.salary.value = res.salary
+    document.frm.id.value = id
+    // res[0].name
 }
 dispData()
